34785bb8ef6fcf2b2c45d74f177dd5a4140a5e1d
David <biggiegun@gmail.com>
2024-03-12 18:25:04 -0500

    Modify Cinema and Seat classes

--- Cinema.js
+++ Cinema.js
@@ -5,23 +5,92 @@ class Cinema {
     seats;
 
     //methods
-    Cinema(rows, seats) {
+    Cinema() {
+    }
+
+    set Seats(seat) {
+        this.setSeat = seat;
+    }
+
+    get Seats() {
+        return `${this.seats}`
+    }
+
+
+    set rows(rows) {
         this.rows = rows;
-        this.seats = seats
+    }
+
+    get rows() {
+        return `${this.rows}`;
     }
 
     showSeatStatus(row, seats, vacancy) {
-        return ((row*seats)-vacancy);
+        return ((row * seats) - vacancy);
     }
 
-    reserveSeat(row, seats) {
+    reserveSeat(rowNumber, seatsNumer, state) {
 
     }
 
 }
 
-const myCinema = new Cinema(5,10);
-console.log("Current busy seats: ")
-myCinema.showSeatStatus(myCinema.seats,myCinema.rows,10);
+class Seat {
 
-export {Cinema};
+    //Atributes
+    rowNumber;
+    seatNumber;
+    isBusy;
+    counter = 0;
+
+    //Methods
+    Seat() {
+    }
+
+    set counter(counter) {
+        this.counter = counter;
+    }
+
+    get counter() {
+        return `${this.counter}`;
+    }
+
+    reserve(rowNumber, seatNumber, isbusy) {
+
+        this.rowNumber = rowNumber;
+        this.seatNumber = seatNumber;
+        this.isBusy = isbusy;
+        this.counter += 1;
+        return this.counter;
+    }
+
+}
+const mySeat = new Seat();
+
+// Set of reserved seats !!
+mySeat.reserve(1, 7, false);
+mySeat.reserve(1, 6, false);
+mySeat.reserve(1, 5, false);
+mySeat.reserve(1, 4, false);
+mySeat.reserve(1, 3, false);
+mySeat.reserve(1, 2, false);
+Seat.counter = mySeat.reserve(1, 1, false);
+console.log("Number of reserved seats:" + Seat.counter);
+
+////////////////////////////////////////////////////////////////////////////
+console.log('<------------------------------------------------------------->');
+const myCinema = new Cinema();
+
+console.log("Number of Cinema seats per row: ");
+Cinema.seats = 5
+console.log(Cinema.seats);
+
+console.log("Number of Cinema rows: ");
+Cinema.rows = 10
+console.log(Cinema.rows);
+
+console.log('Initial capacity ')
+console.log(myCinema.showSeatStatus(Cinema.rows, Cinema.seats, 0));
+
+console.log("Current busy seats: ")
+console.log(myCinema.showSeatStatus(Cinema.rows, Cinema.seats, Seat.counter));
